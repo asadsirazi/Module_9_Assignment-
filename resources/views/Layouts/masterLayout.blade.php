4253d451fb02b050d88c9c54b3ffafd5647da0ae
Asad Hossain Sirazi <144896074+asadsirazi@users.noreply.github.com>
2023-11-26 23:26:19 +0600

    Update masterLayout.blade.php

--- resources/views/Layouts/masterLayout.blade.php
+++ resources/views/Layouts/masterLayout.blade.php
@@ -31,12 +31,12 @@
 
 
 @include('Layouts.footer')
-  <div id="preloader"></div>
+  {{-- <div id="preloader"></div> --}}
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
        <!-- Template Main JS File -->
-    <script src="{{asset('js/main.js')}}"></script>
+   {{-- <script src="{{asset('js/main.js')}}"></script> --}}
   
   </body>
   
