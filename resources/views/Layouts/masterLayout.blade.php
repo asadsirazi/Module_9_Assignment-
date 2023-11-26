6905085878ebb74858eba2d1410e09d4eb850cca
Asad Hossain Sirazi <144896074+asadsirazi@users.noreply.github.com>
2023-11-26 23:00:04 +0600

    Update masterLayout.blade.php

--- resources/views/Layouts/masterLayout.blade.php
+++ resources/views/Layouts/masterLayout.blade.php
@@ -15,15 +15,9 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{asset('vendor/aos/aos.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-  <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
-
+ 
   <!-- Template Main CSS File -->
   <link href="{{asset('css/style.css')}}" rel="stylesheet">
 </head>
@@ -39,18 +33,9 @@
 @include('Layouts.footer')
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Vendor JS Files -->
-    <script src="{{asset('vendor/purecounter/purecounter_vanilla.js')}}"></script>
-    <script src="{{asset('vendor/aos/aos.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('vendor/glightbox/js/glightbox.min.js')}}"></script>
-    <script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-    <script src="{{asset('vendor/swiper/swiper-bundle.min.js')}}"></script>
-    <script src="{{asset('vendor/waypoints/noframework.waypoints.js')}}"></script>
-    <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
-  
-    <!-- Template Main JS File -->
+       <!-- Template Main JS File -->
     <script src="{{asset('js/main.js')}}"></script>
   
   </body>
